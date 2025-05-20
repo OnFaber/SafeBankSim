0581ffdd4ecd9e6157e90ab54b427cd5e1bc2e04
walterMeow <olychko@studenti.laziodigital.it>
2025-05-20 17:31:34 +0200

    aggiunto conto model nel costruttore

--- Models/TransactionModel.cs
+++ Models/TransactionModel.cs
@@ -1,15 +1,17 @@
 using System.ComponentModel.DataAnnotations;
 
-public class TransactionModel {
+public class TransactionModel
+{
     public Guid Id { get; set; }
     public ContoModel Mittente { get; set; } // prendere l'id dalla classe AccountModel
-    public Guid Destinatario { get; set; }
+    public ContoModel Destinatario { get; set; }
     [DataType(DataType.Date)]
     public DateTime Timestamp { get; set; }
 
-    public TransactionModel (Guid id, Guid account, Guid destinatario)
+    public TransactionModel(Guid id, ContoModel mittente, ContoModel destinatario)
     {
         Id = id;
+        Mittente = mittente;
         Destinatario = destinatario;
     }
 }
